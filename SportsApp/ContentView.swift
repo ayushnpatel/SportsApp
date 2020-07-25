c1f66c547b1cbd74e373a94d6fd050ac1f8b9df6
ayushnpatel <ayushnpatel@gmail.com>
2020-07-25 10:54:44 -0400

    Initial Commit

---
+++ SportsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SportsApp
+//
+//  Created by Ayush Patel on 7/25/20.
+//  Copyright © 2020 Ayush Patel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
